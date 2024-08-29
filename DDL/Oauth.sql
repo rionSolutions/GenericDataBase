378372e46a5ac054ae4d6f939217b44c29c40abb
Gabriel Campos <gabrielcampos.dgr@hotmail.com>
2024-08-29 01:01:49 -0300

    add entity

--- DDL/Oauth.sql
+++ DDL/Oauth.sql
@@ -6,11 +6,25 @@ DROP SCHEMA IF EXISTS oauth CASCADE;
 CREATE SCHEMA oauth AUTHORIZATION postgres;
 
 SET schema 	'oauth';
+--- oauth.tb_application definition
+
+-- Drop table
+
+-- DROP TABLE tb_application;
+
+CREATE TABLE tb_application (
+                                application_id varchar(255) NOT NULL,
+                                application_name varchar(255) NULL,
+                                CONSTRAINT tb_application_pkey PRIMARY KEY (application_id)
+);
+
 -- Permissions
 
-GRANT ALL ON SCHEMA oauth TO postgres;
+ALTER TABLE oauth.tb_application OWNER TO postgres;
+GRANT ALL ON TABLE oauth.tb_application TO postgres;
 
--- tb_permissions definition
+
+-- oauth.tb_permissions definition
 
 -- Drop table
 
@@ -24,11 +38,11 @@ CREATE TABLE tb_permissions (
 
 -- Permissions
 
-ALTER TABLE tb_permissions OWNER TO postgres;
-GRANT ALL ON TABLE tb_permissions TO postgres;
+ALTER TABLE oauth.tb_permissions OWNER TO postgres;
+GRANT ALL ON TABLE oauth.tb_permissions TO postgres;
 
 
--- tb_roles definition
+-- oauth.tb_roles definition
 
 -- Drop table
 
@@ -42,11 +56,86 @@ CREATE TABLE tb_roles (
 
 -- Permissions
 
-ALTER TABLE tb_roles OWNER TO postgres;
-GRANT ALL ON TABLE tb_roles TO postgres;
+ALTER TABLE oauth.tb_roles OWNER TO postgres;
+GRANT ALL ON TABLE oauth.tb_roles TO postgres;
 
 
--- tb_permission_roles definition
+-- oauth.tb_permissions_roles_seq definition
+
+-- DROP SEQUENCE oauth.tb_permissions_roles_seq;
+
+CREATE SEQUENCE oauth.tb_permissions_roles_seq
+    INCREMENT BY 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    START 1
+    CACHE 1
+    NO CYCLE;
+
+-- Permissions
+
+ALTER SEQUENCE oauth.tb_permissions_roles_seq OWNER TO postgres;
+GRANT ALL ON SEQUENCE oauth.tb_permissions_roles_seq TO postgres;
+
+
+-- oauth.tb_permissions_seq definition
+
+-- DROP SEQUENCE oauth.tb_permissions_seq;
+
+CREATE SEQUENCE oauth.tb_permissions_seq
+    INCREMENT BY 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    START 1
+    CACHE 1
+    NO CYCLE;
+
+-- Permissions
+
+ALTER SEQUENCE oauth.tb_permissions_seq OWNER TO postgres;
+GRANT ALL ON SEQUENCE oauth.tb_permissions_seq TO postgres;
+
+
+-- oauth.tb_user_session_seq definition
+
+-- DROP SEQUENCE oauth.tb_user_session_seq;
+
+CREATE SEQUENCE oauth.tb_user_session_seq
+    INCREMENT BY 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    START 1
+    CACHE 1
+    NO CYCLE;
+
+-- Permissions
+
+ALTER SEQUENCE oauth.tb_user_session_seq OWNER TO postgres;
+GRANT ALL ON SEQUENCE oauth.tb_user_session_seq TO postgres;
+
+
+-- oauth.tb_app_role definition
+
+-- Drop table
+
+-- DROP TABLE tb_app_role;
+
+CREATE TABLE tb_app_role (
+                             dt_include_regt timestamp(6) NULL,
+                             role_code int8 NULL,
+                             application_id varchar(255) NOT NULL,
+                             CONSTRAINT tb_app_role_pkey PRIMARY KEY (application_id),
+                             CONSTRAINT fk3h4co2gbyd4t09kj57a532lmr FOREIGN KEY (role_code) REFERENCES tb_roles(id),
+                             CONSTRAINT fko2rovqvast64s0jktbamuursw FOREIGN KEY (application_id) REFERENCES tb_application(application_id)
+);
+
+-- Permissions
+
+ALTER TABLE oauth.tb_app_role OWNER TO postgres;
+GRANT ALL ON TABLE oauth.tb_app_role TO postgres;
+
+
+-- oauth.tb_permission_roles definition
 
 -- Drop table
 
@@ -63,117 +152,28 @@ CREATE TABLE tb_permission_roles (
 
 -- Permissions
 
-ALTER TABLE tb_permission_roles OWNER TO postgres;
-GRANT ALL ON TABLE tb_permission_roles TO postgres;
+ALTER TABLE oauth.tb_permission_roles OWNER TO postgres;
+GRANT ALL ON TABLE oauth.tb_permission_roles TO postgres;
 
 
--- tb_user_role definition
-
--- Drop table
-
--- DROP TABLE tb_user_role;
-
-CREATE TABLE tb_user_role (
-                              dt_include_regt timestamp(6) NULL,
-                              role_code int8 NULL,
-                              credentials varchar(255) NOT NULL,
-                              CONSTRAINT tb_user_role_pkey PRIMARY KEY (credentials),
-                              CONSTRAINT fk1xwjj9nj15w3pmt4cyn7tinux FOREIGN KEY (role_code) REFERENCES tb_roles(id)
-);
-
--- Permissions
-
-ALTER TABLE tb_user_role OWNER TO postgres;
-GRANT ALL ON TABLE tb_user_role TO postgres;
-
-
--- tb_session definition
+-- oauth.tb_session definition
 
 -- Drop table
 
 -- DROP TABLE tb_session;
 
 CREATE TABLE tb_session (
+                            active bool NULL,
                             dt_expiration_regt timestamp(6) NULL,
                             dt_include_regt timestamp(6) NULL,
                             id int8 NOT NULL,
-                            app_hash_id VARCHAR(255) NOT NULL ,
-                            credentials varchar(255) NULL,
+                            application_id varchar(255) NULL,
+                            credential_id varchar(255) NULL,
                             CONSTRAINT tb_session_pkey PRIMARY KEY (id),
-                            CONSTRAINT fkm8eadlrxgv6ox75q3eo32yodn FOREIGN KEY (credentials) REFERENCES tb_user_role(credentials)
+                            CONSTRAINT fkeeajx9el8utebm4jxai456oef FOREIGN KEY (application_id) REFERENCES tb_app_role(application_id)
 );
 
 -- Permissions
 
-ALTER TABLE tb_session OWNER TO postgres;
-GRANT ALL ON TABLE tb_session TO postgres;
-
-
--- tb_permissions_roles_seq definition
-
--- DROP SEQUENCE tb_permissions_roles_seq;
-
-CREATE SEQUENCE tb_permissions_roles_seq
-    INCREMENT BY 1
-    MINVALUE 1
-    MAXVALUE 9223372036854775807
-    START 1
-	CACHE 1
-	NO CYCLE;
-
--- Permissions
-
-ALTER SEQUENCE tb_permissions_roles_seq OWNER TO postgres;
-GRANT ALL ON SEQUENCE tb_permissions_roles_seq TO postgres;
-
-
--- tb_permissions_seq definition
-
--- DROP SEQUENCE tb_permissions_seq;
-
-CREATE SEQUENCE tb_permissions_seq
-    INCREMENT BY 1
-    MINVALUE 1
-    MAXVALUE 9223372036854775807
-    START 1
-	CACHE 1
-	NO CYCLE;
-
--- Permissions
-
-ALTER SEQUENCE tb_permissions_seq OWNER TO postgres;
-GRANT ALL ON SEQUENCE tb_permissions_seq TO postgres;
-
-
--- tb_user_session_seq definition
-
--- DROP SEQUENCE tb_user_session_seq;
-
-CREATE SEQUENCE tb_user_session_seq
-    INCREMENT BY 1
-    MINVALUE 1
-    MAXVALUE 9223372036854775807
-    START 1
-	CACHE 1
-	NO CYCLE;
-
--- Permissions
-
-ALTER SEQUENCE tb_user_session_seq OWNER TO postgres;
-GRANT ALL ON SEQUENCE tb_user_session_seq TO postgres;
-
-
--- tb_application definition
-CREATE TABLE tb_application
-(
-    application_name varchar(255) NULL,
-    hash_id          varchar(255) NOT NULL,
-    CONSTRAINT tb_application_pkey PRIMARY KEY (hash_id)
-);
-
--- Permissions
-
-ALTER TABLE oauth.tb_application OWNER TO postgres;
-GRANT
-ALL
-ON TABLE oauth.tb_application TO postgres;
+ALTER TABLE oauth.tb_session OWNER TO postgres;
+GRANT ALL ON TABLE oauth.tb_session TO postgres;
