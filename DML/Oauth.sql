5539ee993f7c716cbe9a1d8e966ec03fec5f515a
Gabriel Campos <gabrielcampos.dgr@hotmail.com>
2024-08-29 01:02:59 -0300

    add entity

--- DML/Oauth.sql
+++ DML/Oauth.sql
@@ -17,5 +17,21 @@ INSERT INTO tb_permission_roles (id, permission_id, role_id) VALUES (nextval('tb
 INSERT INTO tb_permission_roles (id, permission_id, role_id) VALUES (nextval('tb_permissions_roles_seq'), 2, 1);
 INSERT INTO tb_permission_roles (id, permission_id, role_id) VALUES (nextval('tb_permissions_roles_seq'), 5, 1);
 
-INSERT INTO tb_user_role (dt_include_regt, role_code, credentials) VALUES(current_date, 1, 'asnisdy-s1ds8as4d-asdna');
+INSERT INTO tb_permission_roles (id, permission_id, role_id) VALUES (nextval('tb_permissions_roles_seq'), 1, 2);
+INSERT INTO tb_permission_roles (id, permission_id, role_id) VALUES (nextval('tb_permissions_roles_seq'), 2, 2);
+INSERT INTO tb_permission_roles (id, permission_id, role_id) VALUES (nextval('tb_permissions_roles_seq'), 3, 2);
+INSERT INTO tb_permission_roles (id, permission_id, role_id) VALUES (nextval('tb_permissions_roles_seq'), 4, 2);
+INSERT INTO tb_permission_roles (id, permission_id, role_id) VALUES (nextval('tb_permissions_roles_seq'), 5, 2);
+INSERT INTO tb_permission_roles (id, permission_id, role_id) VALUES (nextval('tb_permissions_roles_seq'), 6, 2);
+INSERT INTO tb_permission_roles (id, permission_id, role_id) VALUES (nextval('tb_permissions_roles_seq'), 7, 2);
+
+
+INSERT INTO tb_application (application_name, application_id) VALUES('ORION_LOGIN', 'd9156ff5e1eeb2b288b7499dfcd10bef99b592eaad4bfe3bf80f480082119b48');
+
+INSERT INTO tb_app_role (dt_include_regt, role_code, application_id) VALUES(current_date, 2, 'd9156ff5e1eeb2b288b7499dfcd10bef99b592eaad4bfe3bf80f480082119b48');
+
+
+INSERT INTO tb_session (active, dt_expiration_regt, dt_include_regt, id, application_id, credential_id) VALUES(
+                                                                                                                  false, null, current_date, nextval('tb_user_session_seq'),'d9156ff5e1eeb2b288b7499dfcd10bef99b592eaad4bfe3bf80f480082119b48' , '61d2704d1df69a54bc102a5830049eb7');
+
 
