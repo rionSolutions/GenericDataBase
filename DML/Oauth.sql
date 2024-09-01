3c1ce7922e8de8e10c9e72f6ed99f13b73c2ad35
Gabriel Campos <gabrielcampos.dgr@hotmail.com>
2024-09-01 00:50:55 -0300

    register app and roles

--- DML/Oauth.sql
+++ DML/Oauth.sql
@@ -37,13 +37,13 @@ INSERT INTO tb_permission_roles (id, permission_id, role_id) VALUES (nextval('tb
 
 
 -- register applications
-INSERT INTO tb_application (application_name, application_id) VALUES('ORION_LOGIN', 'd9156ff5e1eeb2b288b7499dfcd10bef99b592eaad4bfe3bf80f480082119b48');
-INSERT INTO tb_application (application_name, application_id) VALUES('ORION_REGISTER', '0f4c1fb77da90980ca57acef52b0ef3cf4d1a8fbc4260855513c84a256b48b4a');
-INSERT INTO tb_application (application_name, application_id) VALUES('ORION_EMAIL_SENDER', 'e5be674ffc1a47eab9a272b31f0ad5de169b11fa10938658c1ce716c6237bac4');
-INSERT INTO tb_application (application_name, application_id) VALUES('ORION_APP_MOBILE', '2edfe44e09c15dd92e0803ed2db1a4fae58151b4df146b4015884cc5565200ef');
+INSERT INTO tb_application (application_name, application_id) VALUES('LOGIN', 'd9156ff5e1eeb2b288b7499dfcd10bef99b592eaad4bfe3bf80f480082119b48');
+INSERT INTO tb_application (application_name, application_id) VALUES('REGISTER', '0f4c1fb77da90980ca57acef52b0ef3cf4d1a8fbc4260855513c84a256b48b4a');
+INSERT INTO tb_application (application_name, application_id) VALUES('EMAIL_SENDER', 'e5be674ffc1a47eab9a272b31f0ad5de169b11fa10938658c1ce716c6237bac4');
+INSERT INTO tb_application (application_name, application_id) VALUES('MOBILE', '2edfe44e09c15dd92e0803ed2db1a4fae58151b4df146b4015884cc5565200ef');
 
-INSERT INTO tb_app_role (dt_include_regt, role_code, application_id) VALUES(current_date, 2, (select application_id from tb_application where application_name = 'ORION_LOGIN'));
-INSERT INTO tb_app_role (dt_include_regt, role_code, application_id) VALUES(current_date, 2, (select application_id from tb_application where application_name = 'ORION_REGISTER'));
-INSERT INTO tb_app_role (dt_include_regt, role_code, application_id) VALUES(current_date, 2, (select application_id from tb_application where application_name = 'ORION_EMAIL_SENDER'));
-INSERT INTO tb_app_role (dt_include_regt, role_code, application_id) VALUES(current_date, 2, (select application_id from tb_application where application_name = 'ORION_APP_MOBILE'));
+INSERT INTO tb_app_role (dt_include_regt, role_code, application_id) VALUES(current_date, 2, (select application_id from tb_application where application_name = 'LOGIN'));
+INSERT INTO tb_app_role (dt_include_regt, role_code, application_id) VALUES(current_date, 2, (select application_id from tb_application where application_name = 'REGISTER'));
+INSERT INTO tb_app_role (dt_include_regt, role_code, application_id) VALUES(current_date, 2, (select application_id from tb_application where application_name = 'EMAIL_SENDER'));
+INSERT INTO tb_app_role (dt_include_regt, role_code, application_id) VALUES(current_date, 2, (select application_id from tb_application where application_name = 'MOBILE'));
 
